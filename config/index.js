fac84e8a2d21419a1cc78a4edaa816b6d001a339
snowdream <yanghui1986527@gmail.com>
2018-04-22 12:09:10 +0800

    Add: Vue build 目录文件夹由 dist 改为 www

--- config/index.js
+++ config/index.js
@@ -49,7 +49,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../www/'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: './',
     assetsPublicPath: './',
 
     /**
