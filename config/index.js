160f0b3151f80705e5518f6061772c257553b41c
snowdream <yanghui1986527@gmail.com>
2018-04-22 17:42:38 +0800

    Add:cube-ui

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 'use strict'
-// Template version: 1.3.1
+// Template version: 1.2.5
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
@@ -33,19 +33,24 @@ module.exports = {
      */
 
     // https://webpack.js.org/configuration/devtool/#development
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'eval-source-map',
 
     // If you have problems debugging vue-files in devtools,
     // set this to false - it *may* help
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
     cacheBusting: true,
 
-    cssSourceMap: true
+    // CSS Sourcemaps off by default because relative paths are "buggy"
+    // with this option, according to the CSS-Loader README
+    // (https://github.com/webpack/css-loader#sourcemaps)
+    // In our experience, they generally work as expected,
+    // just be aware of this issue when enabling this option.
+    cssSourceMap: false,
   },
 
   build: {
     // Template for index.html
-    index:path.resolve(__dirname, '../www/index.html'),
+    index: path.resolve(__dirname, '../www/index.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../www/'),
--- src/main.js
+++ src/main.js
@@ -1,12 +1,71 @@
+import 'amfe-flexible'
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// By default we import all the components.
+// Only reserve the components on demand and remove the rest.
+// Style is always required.
+import {
+  /* eslint-disable no-unused-vars */
+  Style,
+  Button,
+  CheckboxGroup,
+  Radio,
+  Input,
+  Textarea,
+  Select,
+  Switch,
+  Rate,
+  Validator,
+  Form,
+  Loading,
+  Tip,
+  Popup,
+  Toast,
+  Picker,
+  CascadePicker,
+  DatePicker,
+  SegmentPicker,
+  TimePicker,
+  Dialog,
+  ActionSheet,
+  Drawer,
+  Scroll,
+  Slide,
+  IndexList,
+  Swipe,
+  Upload
+} from 'cube-ui'
 import App from './App'
 import router from './router'
 import VueCordova from 'vue-cordova'
-import Cube from 'cube-ui'
 
-Vue.use(Cube)
+Vue.use(Button)
+Vue.use(CheckboxGroup)
+Vue.use(Radio)
+Vue.use(Input)
+Vue.use(Textarea)
+Vue.use(Select)
+Vue.use(Switch)
+Vue.use(Rate)
+Vue.use(Validator)
+Vue.use(Form)
+Vue.use(Loading)
+Vue.use(Tip)
+Vue.use(Toast)
+Vue.use(Picker)
+Vue.use(CascadePicker)
+Vue.use(DatePicker)
+Vue.use(SegmentPicker)
+Vue.use(TimePicker)
+Vue.use(Dialog)
+Vue.use(ActionSheet)
+Vue.use(Drawer)
+Vue.use(Scroll)
+Vue.use(Slide)
+Vue.use(IndexList)
+Vue.use(Swipe)
+Vue.use(Upload)
 Vue.use(VueCordova)
 
 Vue.config.productionTip = false
@@ -15,6 +74,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  components: { App }
 })
